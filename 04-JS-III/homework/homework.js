d59a6eb1bf6dca0b0626e4a7d9333e96e64f7f92
s-garcia-p <jsgp940712@gmail.com>
2020-10-12 10:48:42 -0500

    Modificaciones.

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -35,6 +35,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+    array.push( elemento );
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -42,6 +44,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+   array.unshift(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -64,24 +68,48 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for ( let i = 0; i < array.length; i++ ){
+    if ( array[i] === elemento ){
+      return true;
+    }
+  }
+  return false;
 }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+    var suma = 0;
+  for(var i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+   var resultados = 0
+  for (var i = 0; i < resultadosTest.length; i++) {
+    resultados = resultados + resultadosTest[i];
+    var promedio = resultados / resultadosTest.length;
+  }
+  return promedio;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+   var mayor = 0
+  for (var i = 0; i < numeros.length; i++) {
+    if(numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
 }
 
 function multiplicarArgumentos() {
@@ -89,6 +117,14 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  var producto = 1;
+  if ( arguments.length == 0 ){
+    return 0;
+  }
+  for ( let i = 0; i < arguments.length; i++ ){
+    producto *= arguments[i];
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
